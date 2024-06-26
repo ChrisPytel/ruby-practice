cbaac96c0c9aa4f92187edc753d7fe3b73edc27d
ChrisPytel <chris_pytel@hotmail.com>
2024-06-26 13:37:52 -0400

    add exersises 21, 28, 29

---
+++ ex28.rb
@@ -0,0 +1,16 @@
+# Exercise 28: Boolean and logical operators Practice
+
+# && (and)
+# || (or)
+# ! (not)
+# != (not equal)
+# == (equal)
+# >= (greater-than-equal)
+# <= (less-than-equal)
+
+
+puts "true & true is", true & true;
+puts "true & false is", true & false;
+
+puts "'test' != 'testing'", "test" != "testing";
+puts "comparing the strings 'test' and 'testing'", "test" == "testing" 
---
+++ ex29.rb
@@ -0,0 +1,19 @@
+# Exercise 29: Basic If Statements
+
+people = 20
+cats = 30
+dogs = 15
+
+# end terminates an if / elsif / else statement chain
+
+if people < cats
+  puts "Too many cats! The world is doomed!"
+end
+
+if people > cats
+  puts "Not many cats! The world is saved!"
+end
+
+if people < dogs
+  puts "The world is drooled on!"
+end
