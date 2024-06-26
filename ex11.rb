514195b25450f4da3f4bef8227cc04cb73222ec7
ChrisPytel <chris_pytel@hotmail.com>
2024-06-26 13:37:17 -0400

    add exercises 11, 12, 13, 18, 19

---
+++ ex11.rb
@@ -0,0 +1,16 @@
+# Exercise 11: Asking Questions
+
+
+print "How old are you? "
+age = gets.chomp
+print "How tall are you? "
+height = gets.chomp
+print "How much do you weigh? "
+weight = gets.chomp
+
+puts "So, you're #{age} old, #{height} tall and #{weight} heavy."
+
+#gets.chomp awaits for a string to be typed and enter to be pressed to store the string in memory
+
+# the print command prints the string without requiring a \n (newline) to be added
+# puts requires a \n to register a newline
---
+++ ex13.rb
@@ -0,0 +1,13 @@
+# Exercise 13: Parameters, Unpacking, Variables
+
+arguments = ARGV                # Full object of command line args
+first, second, third = ARGV     # destructured arguments
+
+
+puts "Your first variable is: #{first}"
+puts "Your second variable is: #{second}"
+puts "Your third variable is: #{third}\n\n"
+
+
+print "Our full set of arguements is: ", arguments;
+puts "\n\n"
